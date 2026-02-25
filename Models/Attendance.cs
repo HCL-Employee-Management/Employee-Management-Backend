73afcd753e839ff941167696f1ec4a0e0647f448
Munirathinam_E <munirathinam6724@gmail.com>
2026-02-26 00:14:20 +0530

    Refactored leave and attendance APIs, added DTO for leave request and improved response format to JSON

--- Models/Attendance.cs
+++ Models/Attendance.cs
@@ -11,4 +11,9 @@
 
         public Employee Employee { get; set; }
     }
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
 }
---
+++ Models/LeaveRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayroll.API.Models
+{
+    public class LeaveRequest
+    {
+        public int EmployeeId { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public string Reason { get; set; }
+    }
+}
